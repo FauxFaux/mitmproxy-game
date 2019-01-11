651debde1a1a33da272291bfd762c1c119a75012
Chris West (Faux) <git@goeswhere.com>
2019-01-11 13:10:29 +0000

    everything as lists; can probably build a map with new fixed data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::io;
 use std::iter::Peekable;
 use std::str::Chars;
 
+use failure::bail;
 use failure::ensure;
 use failure::err_msg;
 use failure::Error;
+use serde_json::Value;
 
 #[derive(Debug)]
 struct Block {
@@ -12,16 +15,8 @@ struct Block {
 }
 
 fn take_block(input: &mut Peekable<Chars>) -> Result<Option<Block>, Error> {
-    match input.peek() {
-        Some('}') => {
-            input.next().unwrap();
-            return Ok(Some(Block {
-                sigil: '💖',
-                data: String::new(),
-            }));
-        }
-        None => return Ok(None),
-        _ => (),
+    if input.peek().is_none() {
+        return Ok(None);
     }
 
     let len = input
@@ -42,24 +37,37 @@ fn take_block(input: &mut Peekable<Chars>) -> Result<Option<Block>, Error> {
     Ok(Some(Block { sigil, data }))
 }
 
-fn deconstruct(input: &str, prefix: &str) -> Result<(), Error> {
+fn deconstruct(input: &str, prefix: &str) -> Result<Value, Error> {
     let mut input = input.chars().peekable();
 
+    let mut ret = Vec::new();
+
     while let Some(block) = take_block(&mut input)? {
-        match block.sigil {
-            ']' | '}' => {
-                println!("{}{}:", prefix, block.sigil);
-                deconstruct(&block.data, &format!("{}   ", prefix))?;
-            }
-            _ => println!("{}{:?}", prefix, block),
-        }
+        ret.push(match block.sigil {
+            ']' | '}' => deconstruct(&block.data, &format!("{}   ", prefix))?,
+
+            // ';' means "well known value", I believe. Could be "utf-8" or something.
+            // ',' means "string"
+            // '^' means "unix timestamp with nanos", which can't fit in a JS number
+            // '~' appears to mean "empty string"
+            ';' | ',' | '^' | '~' => Value::String(block.data),
+            '#' => Value::Number(block.data.parse()?),
+            '!' => match block.data.as_str() {
+                "false" => Value::Bool(false),
+                "true" => Value::Bool(true),
+                other => bail!("invalid boolean: {:?}", other),
+            },
+            other => bail!("unimplemented: {} ({:?})", other, block.data),
+        });
     }
 
-    Ok(())
+    Ok(Value::Array(ret))
 }
 
 fn main() -> Result<(), Error> {
-    deconstruct(include_str!("../sample").trim_end(), "")?;
+    let doc = deconstruct(include_str!("../sample").trim_end(), "")?;
+
+    serde_json::to_writer_pretty(io::stdout().lock(), &doc)?;
 
     Ok(())
 }
