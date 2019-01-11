f586e513bdac89954925db487dbb0b3f7543acbf
Chris West (Faux) <git@goeswhere.com>
2019-01-11 12:49:22 +0000

    recursive print

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,65 @@
 use std::iter::Peekable;
 use std::str::Chars;
 
-use failure::bail;
+use failure::ensure;
 use failure::err_msg;
 use failure::Error;
 
-#[derive(Copy, Clone, Debug)]
-enum Sigil {
-    Plain,
-    Array,
-    Object,
-    Comma,
-    Hash,
-    Empty,
-}
-
 #[derive(Debug)]
 struct Block {
-    sigil: Sigil,
+    sigil: char,
     data: String,
 }
 
-fn take_block(input: &mut Peekable<Chars>) -> Result<Block, Error> {
-    let sigil = match *input.peek().ok_or_else(|| err_msg("eof in header: good"))? {
-        c if c.is_numeric() => Sigil::Plain,
-
-        ']' => {
+fn take_block(input: &mut Peekable<Chars>) -> Result<Option<Block>, Error> {
+    match input.peek() {
+        Some('}') => {
             input.next().unwrap();
-            Sigil::Array
+            return Ok(Some(Block {
+                sigil: '💖',
+                data: String::new(),
+            }));
         }
-        ';' => {
-            input.next().unwrap();
-            Sigil::Object
-        }
-        ',' => {
-            input.next().unwrap();
-            Sigil::Comma
-        }
-        '#' => {
-            input.next().unwrap();
-            Sigil::Hash
-        }
-        other => {
-            bail!("unrecognised sigil: {:?}", other);
-        }
-    };
-    let len = input
-        .take_while(|c| c.is_numeric())
-        .collect::<String>();
-
-    if len.is_empty() {
-        return Ok(Block { sigil, data: String::new() });
+        None => return Ok(None),
+        _ => (),
     }
 
-    let len: usize = len.parse()?;
-    let data = input.take(len).collect::<String>();
+    let len = input
+        .take_while(|c| c.is_numeric())
+        .collect::<String>()
+        .parse()?;
 
-    Ok(Block { sigil, data })
+    let data = input.take(len).collect::<String>();
+    ensure!(
+        data.len() == len,
+        "short read, wanted: {}, got: {}",
+        len,
+        data.len()
+    );
+
+    let sigil = input.next().ok_or_else(|| err_msg("no trailing type"))?;
+
+    Ok(Some(Block { sigil, data }))
+}
+
+fn deconstruct(input: &str, prefix: &str) -> Result<(), Error> {
+    let mut input = input.chars().peekable();
+
+    while let Some(block) = take_block(&mut input)? {
+        match block.sigil {
+            ']' | '}' => {
+                println!("{}{}:", prefix, block.sigil);
+                deconstruct(&block.data, &format!("{}   ", prefix))?;
+            }
+            _ => println!("{}{:?}", prefix, block),
+        }
+    }
+
+    Ok(())
 }
 
 fn main() -> Result<(), Error> {
-    let input = include_str!("../sample");
-    let mut input = input.chars().peekable();
-    loop {
-        println!("{:?}", take_block(&mut input)?);
-    }
+    deconstruct(include_str!("../sample").trim_end(), "")?;
+
     Ok(())
 }
