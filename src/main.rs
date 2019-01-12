0461211143e0411b90029caad4b658d31e864e7a
Chris West (Faux) <git@goeswhere.com>
2019-01-12 11:14:00 +0000

    ah right, it's an iterator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
-use std::io::BufRead;
 use std::io::Bytes;
+use std::io::Read;
 use std::iter::Peekable;
 use std::string::FromUtf8Error;
 
@@ -10,6 +10,7 @@ use failure::err_msg;
 use failure::format_err;
 use failure::Error;
 use failure::ResultExt;
+use insideout::InsideOut;
 use itertools::Itertools;
 use serde_json::json;
 use serde_json::Value;
@@ -20,7 +21,7 @@ struct Block {
     data: Vec<u8>,
 }
 
-fn take_block<R: BufRead>(input: &mut Peekable<Bytes<R>>) -> Result<Option<Block>, Error> {
+fn take_block<R: Read>(input: &mut Peekable<Bytes<R>>) -> Result<Option<Block>, Error> {
     match input.peek() {
         Some(Ok(b'\n')) | None => return Ok(None),
         _ => (),
@@ -69,95 +70,93 @@ fn take_block<R: BufRead>(input: &mut Peekable<Bytes<R>>) -> Result<Option<Block
     Ok(Some(Block { sigil, data }))
 }
 
-enum Then<'v> {
-    PushVec(&'v mut Vec<Value>),
-    WriteStdout,
+struct Deconstruct<R: Read> {
+    inner: Peekable<Bytes<R>>,
 }
 
-impl<'v> Then<'v> {
-    fn exec(&mut self, doc: Value) -> Result<(), Error> {
-        match self {
-            Then::PushVec(vec) => vec.push(doc),
-            Then::WriteStdout => serde_json::to_writer_pretty(io::stdout().lock(), &doc)?,
+impl<R: Read> Deconstruct<R> {
+    fn new(inner: R) -> Deconstruct<R> {
+        Deconstruct {
+            inner: inner.bytes().peekable(),
         }
-        Ok(())
     }
 }
 
-fn deconstruct<R: BufRead>(input: R, mut then: Then) -> Result<(), Error> {
-    let mut input = input.bytes().peekable();
+impl<R: Read> Iterator for Deconstruct<R> {
+    type Item = Result<Value, Error>;
 
-    while let Some(block) = take_block(&mut input).with_context(|_| format_err!("reading block"))? {
-        then.exec(match block.sigil {
-            b']' => {
-                let mut contents = Vec::new();
-                deconstruct(io::Cursor::new(block.data), Then::PushVec(&mut contents))
-                    .with_context(|_| format_err!("destructuring array"))?;
-                Value::Array(contents)
-            }
-            b'}' => {
-                let mut contents: Vec<Value> = Vec::new();
-                deconstruct(io::Cursor::new(block.data), Then::PushVec(&mut contents))
-                    .with_context(|_| format_err!("destructuring object"))?;
-                Value::Object(
-                    contents
-                        .into_iter()
-                        .tuples()
-                        .map(|(key, value)| -> Result<_, Error> {
-                            Ok((
-                                key.as_str()
-                                    .ok_or_else(|| {
-                                        format_err!("invalid non-string key: {:?}", key)
-                                    })?
-                                    .to_string(),
-                                value,
-                            ))
-                        })
-                        .collect::<Result<_, Error>>()?,
-                )
-            }
-
-            b',' => match String::from_utf8(block.data) {
-                Ok(s) => Value::String(s),
-                Err(e) => json!({
-                    "base64": base64::encode(e.as_bytes()),
-                }),
-            },
-
-            // ';' means "well known value", I believe. Could be "utf-8" or something.
-            // '^' means "unix timestamp with nanos", which can't fit in a JS number
-            // '~' appears to mean "empty string"
-            a @ b';' | a @ b'^' | a @ b'~' => Value::String(
-                String::from_utf8(block.data)
-                    .map_err(string_error)
-                    .with_context(|_| format_err!("reading string type {:?}", char::from(a)))?,
-            ),
-            b'#' => Value::Number(
-                String::from_utf8(block.data)
-                    .with_context(|_| format_err!("reading number"))?
-                    .parse()?,
-            ),
-            b'!' => match String::from_utf8(block.data)
-                .with_context(|_| format_err!("reading boolean"))?
-                .as_ref()
-            {
-                "false" => Value::Bool(false),
-                "true" => Value::Bool(true),
-                other => bail!("invalid boolean: {:?}", other),
-            },
-            other => bail!(
-                "unimplemented: {} ({:?})",
-                char::from(other),
-                String::from_utf8_lossy(&block.data)
-            ),
-        })?;
+    fn next(&mut self) -> Option<Result<Value, Error>> {
+        take_block(&mut self.inner)
+            .inside_out()
+            .map(|option| option.and_then(|block| expand_block(block)))
     }
+}
 
-    Ok(())
+fn expand_block(block: Block) -> Result<Value, Error> {
+    Ok(match block.sigil {
+        b']' => Value::Array(
+            Deconstruct::new(io::Cursor::new(block.data))
+                .collect::<Result<_, Error>>()
+                .with_context(|_| format_err!("destructuring array"))?,
+        ),
+
+        b'}' => Value::Object(
+            Deconstruct::new(io::Cursor::new(block.data))
+                .collect::<Result<Vec<Value>, Error>>()
+                .with_context(|_| format_err!("destructuring object"))?
+                .into_iter()
+                .tuples()
+                .map(|(key, value)| -> Result<_, Error> {
+                    Ok((
+                        key.as_str()
+                            .ok_or_else(|| format_err!("invalid non-string key: {:?}", key))?
+                            .to_string(),
+                        value,
+                    ))
+                })
+                .collect::<Result<_, Error>>()?,
+        ),
+
+        b',' => match String::from_utf8(block.data) {
+            Ok(s) => Value::String(s),
+            Err(e) => json!({
+                "base64": base64::encode(e.as_bytes()),
+            }),
+        },
+
+        // ';' means "well known value", I believe. Could be "utf-8" or something.
+        // '^' means "unix timestamp with nanos", which can't fit in a JS number
+        // '~' appears to mean "empty string"
+        a @ b';' | a @ b'^' | a @ b'~' => Value::String(
+            String::from_utf8(block.data)
+                .map_err(string_error)
+                .with_context(|_| format_err!("reading string type {:?}", char::from(a)))?,
+        ),
+        b'#' => Value::Number(
+            String::from_utf8(block.data)
+                .with_context(|_| format_err!("reading number"))?
+                .parse()?,
+        ),
+        b'!' => match String::from_utf8(block.data)
+            .with_context(|_| format_err!("reading boolean"))?
+            .as_ref()
+        {
+            "false" => Value::Bool(false),
+            "true" => Value::Bool(true),
+            other => bail!("invalid boolean: {:?}", other),
+        },
+        other => bail!(
+            "unimplemented: {} ({:?})",
+            char::from(other),
+            String::from_utf8_lossy(&block.data)
+        ),
+    })
 }
 
 fn main() -> Result<(), Error> {
-    deconstruct(io::stdin().lock(), Then::WriteStdout)?;
+    for doc in Deconstruct::new(io::stdin().lock()) {
+        serde_json::to_writer_pretty(io::stdout().lock(), &doc?)?;
+    }
     Ok(())
 }
 
@@ -172,15 +171,6 @@ fn string_error(e: FromUtf8Error) -> Error {
 
 #[test]
 fn trivial() -> Result<(), Error> {
-    let mut bodies = Vec::new();
-    deconstruct(
-        io::Cursor::new(
-            b"75:7:headers;61:33:13:Cache-Control,12:no-transform,]20:6:Pragma,8:no-cache,]]}"
-                .to_vec(),
-        ),
-        Then::PushVec(&mut bodies),
-    )?;
-
     assert_eq!(
         vec![json!({
             "headers": [
@@ -188,7 +178,11 @@ fn trivial() -> Result<(), Error> {
                 ["Pragma", "no-cache"],
             ],
         })],
-        bodies
+        Deconstruct::new(io::Cursor::new(
+            b"75:7:headers;61:33:13:Cache-Control,12:no-transform,]20:6:Pragma,8:no-cache,]]}"
+                .to_vec(),
+        ))
+        .collect::<Result<Vec<Value>, Error>>()?
     );
     Ok(())
 }
